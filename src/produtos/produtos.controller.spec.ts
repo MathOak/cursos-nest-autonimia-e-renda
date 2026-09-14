49860e4affca9d613bd7ddcf5ef076e17f4c6b9f
dev.math.prof <45424550+MathOak@users.noreply.github.com>
2026-09-14 11:39:38 -0300

    Aula-02: Configurando os Testes

--- src/produtos/produtos.controller.spec.ts
+++ src/produtos/produtos.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProdutosController } from './produtos.controller';
+import { ProdutosService } from './produtos.service';
 
 describe('ProdutosController', () => {
   let controller: ProdutosController;
@@ -7,6 +8,7 @@ describe('ProdutosController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProdutosController],
+      providers: [ProdutosService]
     }).compile();
 
     controller = module.get<ProdutosController>(ProdutosController);
--- src/produtos/produtos.service.spec.ts
+++ src/produtos/produtos.service.spec.ts
@@ -6,7 +6,7 @@ describe('ProdutosService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProdutosService],
+      providers: [ProdutosService]
     }).compile();
 
     service = module.get<ProdutosService>(ProdutosService);
